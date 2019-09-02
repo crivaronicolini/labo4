61bf57293e4e30117390e75f5e15c098b30d0ba2
marco <crivaronicolini@hotmail.com>
2019-09-02 00:34:44 -0300

    terminada la estructura del analisis, graficos M,T con interpolacion de tabla y polinomio

--- ferro/analisis.py
+++ ferro/analisis.py
@@ -15,8 +15,6 @@ import sys, os
 from scipy.signal import savgol_filter
 import pdb
 
-dir = "/home/marco/Documents/fac/labo4/ferro/mediciones/medicion3/"
-archivos = sorted(os.listdir(dir))
 
 # try:
 #     archivo = sys.argv[1]
@@ -36,14 +34,14 @@ archivos = sorted(os.listdir(dir))
 #     hys.plot(C,D)
 #     curvas = [A,B,C,D]
 #     labels = str(i for i in curvas)
-#     hys.grid(True)
-#     f.tight_layout()
-#     for i in curvas:
-#         cur.plot(np.arange(len(i)),i)
-#         cur.legend(labels)
+#     hys.grid(True)\0
+#     f.tight_layout()\0
+#     for i in curvas:\0
+#         cur.plot(np.arange(len(i)),i)\0
+#         cur.legend(labels)\0
 #     f.show()
     # cur.plot(A,B)
-    # cur.plot(C,D)
+    # cur.plot(C,D)\0
 
 
 def autorotar(P,S):
@@ -71,31 +69,20 @@ def mag(A,B):
     #saco magnetizacion, P = 0 no ocurre porque son muestras discretas,
     #si cruza el eje
     mascara = (A>-0.02) & (A<0.02)
-    print(np.count_nonzero(mascara))
+    # print(np.count_nonzero(mascara))
 
-    cerosP = A[mascara]
+    # cerosP = A[mascara]
     magnetizacion = B[mascara]
     return np.mean(np.absolute(magnetizacion))
-
-
-def tMedia(T):
-    return np.mean(T.T), np.std(T.T)
 
 
 def load(archivo):
     data = np.loadtxt(dir + '/' + archivo, delimiter=',')
 
     #Divido el csv por columnas: Primario, Secundario, Temp, Voltaje de T
-    return np.hsplit(data[:1000,:],4)
+    return np.hsplit(data[:3000,:],4)
 
-#def getT(V):
-#    #funcion para pasar del voltaje a la temp real de termocupla
-#    P, S, _, V = load(archivo[1])
-#    C = np.array[]
-#    T = np.power(V,[0,1,2,3,4])
-def getT(archivo):
-    P, S, U, V = load(archivo)
-    # C = np.array([0.0,0.394501280250e-01,0.236223735980e-04,-0.328589067840e-06])
+def getT(V):
     C = np.array([0.0000000E+00,
          2.5173462E+01,
         -1.1662878E+01,
@@ -106,46 +93,99 @@ def getT(archivo):
         -1.0450598E-03,
         -5.1920577E-04])
 
-    T = C * np.power(V * 1000,range(9))
+    T = C * np.power(V,range(9))
     T = np.sum(T,axis=1)
-    # plt.plot(V * 1000, 'r')
+    return np.mean(-1 * T)
+#     # plt.plot(V * 1000, 'r'8\0
+#     # pdb.set_trace(8\0
+#     plt.plot(U, 'r'8\0
+#     plt.plot(T, 'b'8\0
+#     plt.show(8\0
+
+def temp(A):
+    # P, S, U, V = load(archivo)
+    tabla = np.array([[-220,-6.158],
+                     [-210,-6.035],
+                     [-200,-5.891],
+                     [-190,-5.730],
+                     [-180,-5.550],
+                     [-170,-5.354],
+                     [-160,-5.141],
+                     [-150,-4.913],
+                     [-140,-4.669],
+                     [-130,-4.411],
+                     [-120,-4.138],
+                     [-110,-3.852],
+                     [-100,-3.554],
+                     [-90,-3.243],
+                     [-80,-2.920],
+                     [-70,-2.587],
+                     [-60,-2.243],
+                     [-50,-1.889],
+                     [-40,-1.527],
+                     [-30,-1.156],
+                     [-20,-0.778],
+                     [-10,-0.392],
+                     [0., 0.000]])
+    T = np.interp(A * 1,tabla[:,1],tabla[:,0])
+    return np.mean(T)
+    # plt.plot(V * 10, 'r')
+    # plt.plot(T, 'b')
+    # plt.plot(np.mean(T) * np.ones(len(T)), 'g')
+    # plt.show(8\0
+# print(archivos[0]8\0
+# temp(archivos[0]8\0
+
+def main(archivos, color, medicion):
+    # cantidad = len(archivos)//2 +1
+
     # pdb.set_trace()
-    plt.plot(U, 'r')
-    plt.plot(T, 'b')
-    plt.show()
-
-getT(archivos[0])
-
-
-
-def main(archivos):
+    # color = [i/cantidad for i in range(cantidad)]
     archivos = (i for i in archivos if i.endswith('.csv'))
+    i=0
     for archivo in archivos:
 
-        P, S, T, V = load(archivo)
-
+        P, S, U, V = load(archivo)
+        T = temp(V)
+        if medicion == "medicion3":
+            T = T * 10
+        T = T + 22.
+        # T_ = getT(V)
+        U = np.mean(U)
         #Centro la figura
         P = P - np.mean(P)
         S = S - np.mean(S)
 
-        Pr, Sr = autorotar(P,S)
+        # Pr, Sr = autorotar(P,S)
         m = mag(P,S)
-        mRot = mag(Pr,Sr)
+        # mRot = mag(Pr,Sr)
+        mRot = 0
 
-        t = np.mean(T)
-        resultado = [t, m, mRot]
+        np.set_printoptions(formatter={'float': '{: 0.3f}'.format})
+        resultado = np.array([np.mean(V), T, U,  m])
         # plt.ion()
+
         print(f"{archivo[18:]} tiene resultado {resultado}")
-        plt.plot(t, m, '.', c='b')
-        plt.plot(t, mRot, '.', c='r')
+        # pdb.set_trace()
+        # plt.plot(T, m, '.', c=(0.1, 0.1, color[i]))
+        i+=1
+        plt.plot(T, m, '.', c=color)
+        # plt.plot(U, m, '.', c='b')
 
 
     plt.xlabel('Temperatura (C)')
     plt.ylabel('Magnetizacion')
-    plt.show()
 
-
-
+for i in range(4):
+    dir = "/home/marco/Documents/fac/labo4/ferro/mediciones/medicion" + str(i+1)
+    archivos = sorted(os.listdir(dir))
+    print(f"mediciones de medicion{i+1}")
+    colores = ['r','g','b','k']
+    color = colores[i]
+    plt.ion()
+    main(archivos, color, "medicion" + str(i+1))
+plt.ioff()
+plt.show()
 
 
 # plt.grid(True)
